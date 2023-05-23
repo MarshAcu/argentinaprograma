6da833ebd467a7bc3711c4327e5cbbb3866e97f6
Marshall <marko.benq@gmail.com>
2023-05-23 19:32:30 -0300

    prueba resta, multiplicacion

--- practico n0/multiplicacion.js
+++ practico n0/multiplicacion.js
@@ -3,3 +3,5 @@ function multiplicacion(num1, num2, num3) {
     resultado = num1 * num2 * num3;
     console.log(resultado);
 }
+
+multiplicacion(2, 2, 2);
--- practico n0/resta.js
+++ practico n0/resta.js
@@ -3,3 +3,5 @@ function restar(minuendo, sustraendo) {
     resultado = minuendo - sustraendo;
     console.log(resultado);
 }
+
+restar(10, 5);
