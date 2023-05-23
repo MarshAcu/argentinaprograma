d9ae993b5efea3fcd1ef7ef9def60a7660df4a6e
Marshall <marko.benq@gmail.com>
2023-05-23 19:43:40 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,6 @@
+function calcularAreaRectangulo(base, altura) {
+    resultado = base * altura;
+    console.log(resultado);
+}
+
+calcularAreaRectangulo(12.5, 5.3);
---
+++ promedio3.js
@@ -0,0 +1,6 @@
+function calcularPromedioDeTres(number1, number2, number3) {
+    resultado = (number1 + number2 + number3) / 3;
+    console.log(resultado);
+}
+
+calcularPromedioDeTres(-1.2, 5.3, -3.3);
